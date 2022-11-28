25a0873aca9ab76eee5e217bd37118fbd54c16a2
Nicolas Origlia <noriglia@gmail.com>
2022-11-28 17:12:39 -0300

    lista guardada

---
+++ Lista-Obj.js
@@ -0,0 +1,9 @@
+const desarrollador = [ 'nicolas', 24, true,  1998, 
+    libro= {
+        titulo : 'Padre Rico, Padre Pobre',
+        autor : 'Robert Kiyosaki y Sharon Lechter',
+        fhecha: 1997,
+        url: 'https://www.economicas.unsa.edu.ar/afinan/informacion_general/book/padre-rico-padre-pobre.pdf'
+    }
+]
+console.log(desarrollador);
